aef336c866c5d85ceffdb72eaef7295b6737d161
Tim Lauridsen <tla@rasmil.dk>
2019-04-29 14:27:24 +0200

    Make adding repo more generic

--- repo-setup.sh
+++ repo-setup.sh
@@ -7,23 +7,29 @@ APT_SRC_DIR="/etc/apt/sources.list.d"
 
 add_google_cloud_sdk() {
     # Add Google Cloud SDK repo
-    GOOGLE_CLOUD_REPO="google-cloud-sdk.list"
-    if [ ! -f $APT_SRC_DIR/$GOOGLE_CLOUD_REPO ]; then
+    REPO="google-cloud-sdk.list"
+    KEY_URL="https://packages.cloud.google.com/apt/doc/apt-key.gpg"
+    if [ ! -f $APT_SRC_DIR/$REPO ]; then
         echo "--> Adding google-cloud-sdk repo (apt)"
         #CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
         CLOUD_SDK_REPO="cloud-sdk-cosmic" # hardcode cosmic, no sdk repo for dicso yet
-        echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a $APT_SRC_DIR/$GOOGLE_CLOUD_REPO
-        curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+        SOURCE="deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main"
+        echo "$SOURCE" | sudo tee -a $APT_SRC_DIR/$REPO &>/dev/null
+        echo -n "--> Importing Key from : $KEY_URL "
+        curl --silent $KEY_URL | sudo apt-key add -
     fi
 }
 
 add_google_chrome() {
     # Add Google Chrome SDK repo
-    GOOGLE_CROME_REPO="google-chrome.list"
-    if [ ! -f $APT_SRC_DIR/GOOGLE_CROME_REPO ]; then
+    REPO="google-chrome.list"
+    KEY_URL="https://dl.google.com/linux/linux_signing_key.pub"
+    if [ ! -f $APT_SRC_DIR/$REPO ]; then
         echo "--> Adding google-chrome repo (apt)"
-        echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a $APT_SRC_DIR/$GOOGLE_CLOUD_REPO
-        curl https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+        SOURCE="deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" 
+        echo "$SOURCE" | sudo tee -a $APT_SRC_DIR/$REPO &>/dev/null
+        echo -n "--> Importing Key from : $KEY_URL "
+        curl --silent $KEY_URL | sudo apt-key add -
     fi
 }
 
